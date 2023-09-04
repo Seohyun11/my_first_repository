f88622cd1dd1d61732fd164232a1011cc38ffa46
seohyun11 <sagelsh@catholic.ac.kr>
2023-09-04 17:51:10 +0900

    add eddittest

---
+++ eddittest.py
@@ -0,0 +1,26 @@
+a="hello"
+b="world"
+print(a,b)
+
+print(a)
+
+print(b)
+
+print(b,a)
+
+number_school = 2394584903
+str_name="python"
+print(number_school)
+print(str_name)
+
+bool_view = False
+print(bool_view)
+
+flt_pi=3.14
+print(flt_pi)
+
+char_element='a'
+
+chars= "hello world"
+
+print("number:",number_school, "name:",str_name,"bool:",bool_view,"float:",flt_pi,"charcters:",char_element)
